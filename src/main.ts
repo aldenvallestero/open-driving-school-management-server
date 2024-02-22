a1c0a6d788c46f7d5edf53ef24c91b71195ffec8
Alden Vallestero <alden.v@agtuary.com>
2024-02-22 17:59:20 +0800

    🔧 Add Dynamic PORT for AZURE

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(1337);
+  const PORT = process.env.PORT || 8080;
+  await app.listen(PORT);
 }
 bootstrap();
