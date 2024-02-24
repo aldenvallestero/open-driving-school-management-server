c2b288084670fabce499aba99e3128b6d9f16b2b
Alden Vallestero <alden.v@agtuary.com>
2024-02-24 22:24:53 +0800

    💚 Add Website Port

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const PORT = process.env.PORT || process.env.port || 8080;
+  const PORT =
+    process.env.WEBSITE_PORT ||
+    process.env.webste_port ||
+    process.env.PORT ||
+    process.env.port ||
+    8080;
   await app.listen(PORT, () => {
     console.log('Server started running on port', PORT);
   });
