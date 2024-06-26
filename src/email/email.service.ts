ccb3ddf3e1c60918c2c37444ac3542cb3daf24da
aldenvallestero <betas-pupa0p@icloud.com>
2024-06-26 22:06:15 +0800

    ✨ Add send to EmailService

--- src/email/email.service.ts
+++ src/email/email.service.ts
@@ -1,4 +1,36 @@
+import * as nodemailer from 'nodemailer';
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class EmailService {}
+export class EmailService {
+  transporter: any;
+
+  constructor() {
+    this.transporter = nodemailer.createTransport({
+      service: 'Gmail',
+      host: 'smtp.gmail.com',
+      port: 465,
+      secure: true,
+      auth: {
+        user: 'aldenvallestero.official@gmail.com',
+        pass: 'eddoxhpedcfifire',
+      },
+    });
+  }
+
+  send(from: string, to: string, subject: string, text: string): void {
+    try {
+      new Promise(() => {
+        this.transporter.sendMail({
+          from,
+          to,
+          subject,
+          text,
+        });
+      });
+    } catch (error) {
+      console.error(`EmailService.sendEmail: ${error}`);
+      return;
+    }
+  }
+}
